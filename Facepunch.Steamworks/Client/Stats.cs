c4e4bdb749ebccbd4d83b615bd247f4190bde65c
Garry Newman <garrynewman@gmail.com>
2016-07-15 11:26:27 +0100

    Added stats

---
+++ Facepunch.Steamworks/Client/Stats.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Facepunch.Steamworks
+{
+    public partial class Client : IDisposable
+    {
+        Stats _stats;
+
+        public Stats Stats
+        {
+            get
+            {
+                if ( _stats == null )
+                    _stats = new Stats( this );
+
+                return _stats;
+            }
+        }
+    }
+
+    public class Stats
+    {
+        internal Client client;
+
+        internal Stats( Client c )
+        {
+            client = c;
+        }
+    }
+}
