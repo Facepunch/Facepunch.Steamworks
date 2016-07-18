865f85bc53191fe78513501c79baca36b3fda2f1
Garry Newman <garrynewman@gmail.com>
2016-07-18 12:33:13 +0100

    SteamFriend, Image

--- Facepunch.Steamworks/Client/Friends.cs
+++ Facepunch.Steamworks/Client/Friends.cs
@@ -21,6 +21,34 @@ namespace Facepunch.Steamworks
         }
     }
 
+    public class SteamFriend
+    {
+        /// <summary>
+        /// Steam Id
+        /// </summary>
+        public ulong Id { get; internal set; }
+
+        /// <summary>
+        ///  Return true if is a friend. Returns false if blocked, request etc.
+        /// </summary>
+        public bool IsFriend { get; internal set; }
+
+        /// <summary>
+        /// Returns true if this friend is playing a game (and we know about it)
+        /// </summary>
+        public bool IsPlaying { get; internal set; }
+
+        /// <summary>
+        /// If they're current in a game, what are they playing?
+        /// </summary>
+        public int CurrentAppId;
+
+        /// <summary>
+        /// Their current display name
+        /// </summary>
+        public string Name;
+    }
+
     public class Friends
     {
         internal Client client;
@@ -35,6 +63,81 @@ namespace Facepunch.Steamworks
             client.native.friends.RequestUserInformation( steamid, true );
 
             return client.native.friends.GetFriendPersonaName( steamid );
+        }
+
+        private List<SteamFriend> _allFriends;
+
+        public IEnumerable<SteamFriend> All
+        {
+            get
+            {
+                if ( _allFriends == null )
+                {
+                    _allFriends = new List<SteamFriend>();
+                    Refresh();
+                }
+
+                return _allFriends;
+            }
+        }
+
+        public void Refresh()
+        {
+            _allFriends.Clear();
+
+            //client.native.friends.GetFriendCount( 0 );
+
+        }
+
+        public enum AvatarSize
+        {
+            /// <summary>
+            /// Should be 32x32 - but make sure to check!
+            /// </summary>
+            Small,
+
+            /// <summary>
+            /// Should be 64x64 - but make sure to check!
+            /// </summary>
+            Medium,
+
+            /// <summary>
+            /// Should be 184x184 - but make sure to check!
+            /// </summary>
+            Large
+        }
+
+        public Image GetAvatar( AvatarSize size, ulong steamid )
+        {
+            var imageid = 0;
+
+            switch ( size )
+            {
+                case AvatarSize.Small:
+                    imageid = client.native.friends.GetSmallFriendAvatar( steamid );
+                    break;
+                case AvatarSize.Medium:
+                    imageid = client.native.friends.GetMediumFriendAvatar( steamid );
+                    break;
+                case AvatarSize.Large:
+                    imageid = client.native.friends.GetLargeFriendAvatar( steamid );
+                    break;
+            }
+
+            uint width = 0;
+            uint height = 0;
+
+            if ( imageid != 0)
+            {
+                client.native.utils.GetImageSize( imageid, ref width, ref height );
+            }
+
+            return new Image()
+            {
+                Id = imageid,
+                Width = (int)width,
+                Height = (int)height
+            };
         }
     }
 }
---
+++ Facepunch.Steamworks/Client/Image.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Facepunch.Steamworks
+{
+    public class Image
+    {
+        public int Id { get; internal set; }
+        public int Width { get; internal set; }
+        public int Height { get; internal set; }
+
+
+    }
+}
