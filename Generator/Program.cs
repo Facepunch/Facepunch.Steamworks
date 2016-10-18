2680e4c45a2dd0238e6e78bd07a7842f5b3e2447
Garry Newman <garrynewman@gmail.com>
2016-10-18 16:34:28 +0100

    Added Generator

---
+++ Generator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Generator
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            var content = System.IO.File.ReadAllText( "steam_api.json" );
+            var def = Newtonsoft.Json.JsonConvert.DeserializeObject<SteamApiDefinition>( content );
+
+            Console.WriteLine( content );
+
+            foreach ( var d in def.typedefs )
+            {
+                Console.WriteLine( "{0} = {1}", d.Name, d.Type );
+            }
+
+            foreach ( var d in def.enums )
+            {
+                Console.WriteLine( "{0} = {1}", d.Name, d.Values );
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
---
+++ Generator/SteamApiDefinition.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Generator
+{
+    public class SteamApiDefinition
+    {
+        public class TypeDef
+        {
+            [JsonProperty( PropertyName = "typedef" )]
+            public string Name { get; set; }
+            [JsonProperty( PropertyName = "type" )]
+            public string Type { get; set; }
+        }
+
+        public TypeDef[] typedefs { get; set; }
+
+        public class EnumDef
+        {
+            public class EnumValue
+            {
+                [JsonProperty( PropertyName = "name" )]
+                public string Name { get; set; }
+                [JsonProperty( PropertyName = "value" )]
+                public string Value { get; set; }
+            }
+
+            [JsonProperty( PropertyName = "enumname" )]
+            public string Name { get; set; }
+            [JsonProperty( PropertyName = "values" )]
+            public EnumValue[] Values { get; set; }
+        }
+
+        public EnumDef[] enums { get; set; }
+
+        public class StructDef
+        {
+            public class StructFields
+            {
+                [JsonProperty( PropertyName = "fieldname" )]
+                public string Name { get; set; }
+                [JsonProperty( PropertyName = "fieldtype" )]
+                public string Type { get; set; }
+            }
+
+            [JsonProperty( PropertyName = "struct" )]
+            public string Name { get; set; }
+            [JsonProperty( PropertyName = "fields" )]
+            public StructFields[] Fields { get; set; }
+        }
+
+        public StructDef[] structs { get; set; }
+
+        public class MethodDef
+        {
+            public class ParamType
+            {
+                [JsonProperty( PropertyName = "paramname" )]
+                public string Name { get; set; }
+                [JsonProperty( PropertyName = "paramtype" )]
+                public string Type { get; set; }
+            }
+
+            [JsonProperty( PropertyName = "classname" )]
+            public string ClassName { get; set; }
+            [JsonProperty( PropertyName = "methodname" )]
+            public string Name { get; set; }
+            [JsonProperty( PropertyName = "returntype" )]
+            public string ReturnType { get; set; }
+            [JsonProperty( PropertyName = "params" )]
+            public ParamType[] Params { get; set; }
+        }
+
+        public MethodDef[] methods { get; set; }
+    }
+}
