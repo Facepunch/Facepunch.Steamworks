6a5603a12132e3b2c3b8d12ab6c05310f105cdea
Garry Newman <garrynewman@gmail.com>
2016-11-11 10:44:02 +0000

    Added Utility.Epoch

--- Facepunch.Steamworks/Utility.cs
+++ Facepunch.Steamworks/Utility.cs
@@ -14,5 +14,30 @@ namespace Facepunch.Steamworks
                    ( ( x & 0x00ff0000 ) >> 8 ) +
                    ( ( x & 0xff000000 ) >> 24 );
         }
+
+        static internal class Epoch
+        {
+            private static readonly DateTime epoch = new DateTime( 1970, 1, 1, 0, 0, 0, DateTimeKind.Utc );
+
+            /// <summary>
+            /// Returns the current Unix Epoch
+            /// </summary>
+            public static int Current
+            {
+                get
+                {
+                    return (int)( DateTime.UtcNow.Subtract( epoch ).TotalSeconds );
+                }
+            }
+
+            /// <summary>
+            /// Convert an epoch to a datetime
+            /// </summary>
+            public static DateTime ToDateTime( decimal unixTime )
+            {
+                return epoch.AddSeconds( (long)unixTime );
+            }
+
+        }
     }
 }
