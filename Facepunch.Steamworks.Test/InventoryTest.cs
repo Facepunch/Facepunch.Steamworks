396be12edd5a80e264943ec0d700d4b9a991442e
Garry Newman <garrynewman@users.noreply.github.com>
2019-04-27 15:56:11 +0100

    Create InventoryTest.cs

---
+++ Facepunch.Steamworks.Test/InventoryTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Steamworks.Data;
+
+namespace Steamworks
+{
+    [TestClass]
+    [DeploymentItem( "steam_api64.dll" )]
+    public class InventoryTest
+	{
+		[TestMethod]
+        public async Task GetDefinitions()
+        {
+
+		}
+	}
+
+}
