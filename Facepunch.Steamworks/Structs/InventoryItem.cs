f06b4b431badecb8201f83433310b60e6ca8e2e6
Garry Newman <garrynewman@users.noreply.github.com>
2019-05-16 15:02:16 +0100

    InventoryItem.Flags uses SteamItemFlags

--- Facepunch.Steamworks/Structs/InventoryItem.cs
+++ Facepunch.Steamworks/Structs/InventoryItem.cs
@@ -9,7 +9,7 @@ namespace Steamworks
 	{
 		internal InventoryItemId _id;
 		internal InventoryDefId _def;
-		internal int _flags;
+		internal SteamItemFlags _flags;
 		internal ushort _quantity;
 		internal Dictionary<string, string> _properties;
 
@@ -31,19 +31,19 @@ namespace Steamworks
 		/// This item is account-locked and cannot be traded or given away. 
 		/// This is an item status flag which is permanently attached to specific item instances
 		/// </summary>
-		public bool IsNoTrade => (_flags & 1 << 0) != 0;
+		public bool IsNoTrade => _flags.HasFlag( SteamItemFlags.NoTrade );
 
 		/// <summary>
 		/// The item has been destroyed, traded away, expired, or otherwise invalidated. 
 		/// This is an action confirmation flag which is only set one time, as part of a result set.
 		/// </summary>
-		public bool IsRemoved => (_flags & 1 << 8) != 0;
+		public bool IsRemoved => _flags.HasFlag( SteamItemFlags.Removed );
 
 		/// <summary>
 		/// The item quantity has been decreased by 1 via ConsumeItem API. 
 		/// This is an action confirmation flag which is only set one time, as part of a result set.
 		/// </summary>
-		public bool IsConsumed => (_flags & 1 << 9) != 0;
+		public bool IsConsumed => _flags.HasFlag( SteamItemFlags.Consumed );
 
 		/// <summary>
 		/// Consumes items from a user's inventory. If the quantity of the given item goes to zero, it is permanently removed.
@@ -91,7 +91,7 @@ namespace Steamworks
 			{
 				_id = details.ItemId,
 				_def = details.Definition,
-				_flags = details.Flags,
+				_flags = (SteamItemFlags) details.Flags,
 				_quantity = details.Quantity
 			};
 
