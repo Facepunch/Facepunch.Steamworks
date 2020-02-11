1630aaae22824e2da7f0fd9f6de812f15365e104
Garry Newman <garrynewman@users.noreply.github.com>
2020-02-11 10:16:41 +0000

    Adding SteamMatchmakingServers interface (for uniformity)

---
+++ Facepunch.Steamworks/SteamMatchmakingServers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Steamworks.Data;
+
+namespace Steamworks
+{
+	/// <summary>
+	/// Functions for clients to access matchmaking services, favorites, and to operate on game lobbies
+	/// </summary>
+	public class SteamMatchmakingServers : SteamClass
+	{
+		internal static ISteamMatchmakingServers Internal;
+		internal override SteamInterface Interface => Internal;
+
+		internal override void InitializeInterface()
+		{
+			Internal = new ISteamMatchmakingServers();
+		}
+	}
+}
