93e21a5f795e16a42087ff7b7f1d69098010924e
Garry Newman <garrynewman@users.noreply.github.com>
2020-02-27 09:21:38 +0000

    Generate a dictionary of CallbackType's and the actual typeof the callback struct

--- Generator/CodeWriter/CustomEnums.cs
+++ Generator/CodeWriter/CustomEnums.cs
@@ -10,12 +10,24 @@ namespace Generator
     {
         void CustomEnums()
         {
-			StartBlock( "enum CallbackType" );
+			StartBlock( "public enum CallbackType" );
 			foreach ( var c in def.callback_structs.OrderBy( x => x.CallbackId ) )
 			{
 				WriteLine( $"{c.Name.Replace( "_t", "" ) } = {c.CallbackId}," );
 			}
 			EndBlock();
-        }
+
+			StartBlock( "internal static partial class CallbackTypeFactory" );
+			StartBlock( "internal static System.Collections.Generic.Dictionary<CallbackType, System.Type> All = new System.Collections.Generic.Dictionary<CallbackType, System.Type>" );
+			foreach ( var c in def.callback_structs.OrderBy( x => x.CallbackId ) )
+			{
+				if ( Cleanup.IsDeprecated( c.Name ) )
+					Write( "// " );
+
+				WriteLine( $"{{ CallbackType.{c.Name.Replace( "_t", "" ) }, typeof( {Cleanup.ConvertType(c.Name)} )}}," );
+			}
+			EndBlock( ";" );
+			EndBlock();
+		}
     }
 }
