a3bd199b115507df90356f32fe505f0c20ec646d
kamyker <kamyker@o2.pl>
2020-05-20 22:32:51 +0200

    fix for count == 0

--- Facepunch.Steamworks/Structs/UgcItem.cs
+++ Facepunch.Steamworks/Structs/UgcItem.cs
@@ -228,11 +228,12 @@ namespace Steamworks.Ugc
 											.GetPageAsync( 1 );
 
 			if ( !file.HasValue ) return null;
-			if ( file.Value.ResultCount == 0 ) return null;
+			using ( file.Value )
+			{
+				if ( file.Value.ResultCount == 0 ) return null;
 
-			var item = file.Value.Entries.First();
-			file.Value.Dispose();
-			return item;
+				return file.Value.Entries.First();
+			}
 		}
 
 		internal static Item From( SteamUGCDetails_t details )
