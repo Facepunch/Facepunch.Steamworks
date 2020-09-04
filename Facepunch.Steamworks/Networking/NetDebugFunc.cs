73b5c9ceb69de8b688320b8054192c696c73d041
Garry Newman <garrynewman@gmail.com>
2020-09-04 09:53:07 +0100

    Manually create SteamNet delegates

--- Facepunch.Steamworks/Networking/NetDebugFunc.cs
+++
@@ -1,9 +0,0 @@
-﻿using Steamworks.Data;
-using System;
-using System.Runtime.InteropServices;
-
-namespace Steamworks.Data
-{
-	[UnmanagedFunctionPointer( Platform.CC )]
-	delegate void NetDebugFunc( [In] NetDebugOutput nType, [In] IntPtr pszMsg );
-}
