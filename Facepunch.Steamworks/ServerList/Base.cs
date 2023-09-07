f8960c8971aad2e03eabed7153ec9a18a1e3135a
Jake_Rich <jake@facepunchstudios.com>
2023-09-06 21:46:41 -0400

    Put all servers that failed to respond to the query into a "Unqueried" list intead of "Unresponsive"

--- Facepunch.Steamworks/ServerList/Base.cs
+++ Facepunch.Steamworks/ServerList/Base.cs
@@ -41,6 +41,7 @@ namespace Steamworks.ServerList
 		/// </summary>
 		public List<ServerInfo> Unresponsive = new List<ServerInfo>();
 
+		public List<ServerInfo> Unqueried = new List<ServerInfo>();
 
 		public Base()
 		{
@@ -176,8 +177,10 @@ namespace Steamworks.ServerList
 		{
 			watchList.RemoveAll( x =>
 			{
-				var info = Internal.GetServerDetails( request, x );
-				OnServer( ServerInfo.From( info ), info.HadSuccessfulResponse );
+				var details = Internal.GetServerDetails( request, x );
+				var info = ServerInfo.From( details );
+				info.Ping = int.MaxValue;
+				Unqueried.Add( info );
 				return true;
 			} );
 		}
