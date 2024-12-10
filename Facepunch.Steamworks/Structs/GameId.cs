520ac1ca5130266f3b057e29d4cb0956c1060ddc
Rohan Singh <rohan-singh@hotmail.com>
2024-12-10 15:03:47 -0500

    Add Friend.Nickname to get Steam nicknames for players Expose FriendGameInfo.GameID so you can see what appID friends are playing

--- Facepunch.Steamworks/Structs/GameId.cs
+++ Facepunch.Steamworks/Structs/GameId.cs
@@ -1,25 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Runtime.InteropServices;
-using System.Text;
-
 
 namespace Steamworks.Data
 {
-	public struct GameId
+	public enum GameIdType : byte
 	{
-		// TODO - Be able to access these vars
-
+		App = 0,
+		GameMod = 1,
+		Shortcut = 2,
+		P2P = 3,
+	}
+	
+	public struct GameId : IEquatable<GameId>
+	{
 		/*
-		 
-		enum EGameIDType
-		{
-			k_EGameIDTypeApp		= 0,
-			k_EGameIDTypeGameMod	= 1,
-			k_EGameIDTypeShortcut	= 2,
-			k_EGameIDTypeP2P		= 3,
-		};
-
 		# ifdef VALVE_BIG_ENDIAN
 			unsigned int m_nModID : 32;
 			unsigned int m_nType : 8;
@@ -30,7 +23,30 @@ namespace Steamworks.Data
 			unsigned int m_nModID : 32;
 		#endif
 		*/
+		
+		// 0xAAAAAAAA_BBCCCCCC
+		// A = m_nModID
+		// B = m_nType
+		// C = m_nAppID
 		public ulong Value;
+
+		public GameIdType Type
+		{
+			get => (GameIdType)(byte)( Value >> 24 );
+			set => Value = ( Value & 0xFFFFFFFF_00FFFFFF ) | ( (ulong)(byte)value << 24 );
+		}
+		
+		public uint AppId
+		{
+			get => (uint)( Value & 0x00000000_00FFFFFF );
+			set => Value = ( Value & 0xFFFFFFFF_FF000000 ) | (value & 0x00000000_00FFFFFF);
+		}
+		
+		public uint ModId
+		{
+			get => (uint)( Value >> 32 );
+			set => Value = ( Value & 0x00000000_FFFFFFFF ) | ( (ulong)value << 32 );
+		}
 
 		public static implicit operator GameId( ulong value )
 		{
@@ -40,6 +56,31 @@ namespace Steamworks.Data
 		public static implicit operator ulong( GameId value )
 		{
 			return value.Value;
+		}
+
+		public bool Equals(GameId other)
+		{
+			return Value == other.Value;
+		}
+
+		public override bool Equals(object obj)
+		{
+			return obj is GameId other && Equals(other);
+		}
+
+		public override int GetHashCode()
+		{
+			return Value.GetHashCode();
+		}
+
+		public static bool operator ==(GameId left, GameId right)
+		{
+			return left.Equals(right);
+		}
+
+		public static bool operator !=(GameId left, GameId right)
+		{
+			return !left.Equals(right);
 		}
 	}
 }
